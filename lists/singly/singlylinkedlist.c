048829e7a48db7e078cbdeef62254ff047ad9a88
LorenzoPalacios1 <lp434717@gmail.com>
2024-11-16 18:54:55 -0600

    refactoring
    Signed-off-by: LorenzoPalacios1 <lp434717@gmail.com>

--- lists/singly/singlylinkedlist.c
+++ lists/singly/singlylinkedlist.c
@@ -3,56 +3,86 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdalign.h>
 
-typedef unsigned char byte_t;
+typedef unsigned char byte;
 
-static s_linked_list *s_list_alloc(const size_t alloc_size) {
-  return malloc(alloc_size + sizeof(s_linked_list));
+static inline size_t node_value_pair_size(const sl_list *const list) {
+  const size_t VALUE_SIZE = list->value_size;
+  return sizeof(sll_node) + VALUE_SIZE;
 }
 
-s_linked_list *_new_s_list(const void *const data, const size_t num_elems,
-                           const size_t elem_size) {
-  s_linked_list *const list = new_empty_s_list(num_elems, elem_size);
+static inline size_t list_length_overall(const sl_list *const list) {
+  const size_t ALLOC = list->data_allocation;
+  const size_t PAIR_SIZE = node_value_pair_size(list);
+  return ALLOC / PAIR_SIZE;
+}
+
+static inline size_t list_length_used(const sl_list *const list) {
+  return list->end - list->start;
+}
+
+static inline size_t list_length_unused(const sl_list *const list) {
+  const size_t OVERALL_LENGTH = list_length_overall(list);
+  const size_t UNUSED_LENGTH = OVERALL_LENGTH - list->length;
+  return UNUSED_LENGTH;
+}
+
+static inline void *get_values(sl_list *const list) { return list + 1; }
+
+static inline sll_node *get_nodes(sl_list *const list) {
+  byte *const VALUES = get_values(list);
+  const size_t VALUE_SIZE = list->value_size;
+  const size_t LIST_LENGTH = list_length_overall(list);
+  return (void *)(VALUES + LIST_LENGTH * VALUE_SIZE);
+}
+
+static inline size_t calc_padding_bytes(const size_t values_alloc) {
+  const size_t SIZE_DIFF = values_alloc % alignof(sll_node);
+  if (SIZE_DIFF == 0) return 0;
+  const size_t PADDING_BYTES = alignof(sll_node) - SIZE_DIFF;
+  return PADDING_BYTES;
+}
+
+sl_list *init_sl_list(const size_t num_nodes, const size_t value_size) {
+
+}
+
+sl_list *_new_sl_list(const void *const data, const size_t num_elems,
+                      const size_t elem_size) {
+  sl_list *const list = init_sl_list(num_elems, elem_size);
   if (list == NULL) return NULL;
 
-  /* The nodes are stored past the list header. */
-  const size_t NODE_SIZE = elem_size + sizeof(s_linked_node);
-  s_linked_node *const nodes_mem = (void *)(list + 1);
+  byte *const VALUES_MEM = get_values(list);
+  sll_node *const NODES_MEM = get_nodes(list);
 
-  s_linked_node *cur_node = nodes_mem;
-  s_linked_node *prev_node = NULL;
-  /* The first element of `data` will always be the first node in the list. */
-  list->start_node = nodes_mem;
+  sll_node *cur_node = NODES_MEM;
+  sll_node *prev_node = NULL;
   for (size_t i = 0; i < num_elems; i++) {
-    const void *const cur_elem = (char *)data + i * elem_size;
-    /* The value stored at the node should be ahead of the node header. */
-    cur_node->value = nodes_mem + 1;
-    memcpy(cur_node->value, cur_elem, elem_size);
-    nodes_mem->next = NULL;
-    if (prev_node != NULL) prev_node->next = cur_node;
-    cur_node = (void *)((byte_t *)cur_node + NODE_SIZE);
+    const void *const cur_elem = (byte *)data + i * elem_size;
+    void *const cur_value = VALUES_MEM + i * elem_size;
+    memcpy(cur_value, cur_elem, elem_size);
+    cur_node->value_index = i;
+    cur_node->next_node_index = 0;
+    if (prev_node != NULL) prev_node->next_node_index = i - 1;
+    /*
+     * Incrementing to the next node since `cur_node` is a copy of the pointer
+     * to `NODES_MEM`.
+     */
+    cur_node++;
   }
-  /* The last element of `data` will always be the last node in the list. */
-  list->end_node = cur_node;
-
-  return list;
-}
-
-s_linked_list *new_empty_s_list(const size_t num_elems,
-                                const size_t elem_size) {
-  const size_t ALLOC_SIZE = elem_size * num_elems;
-  s_linked_list *list = s_list_alloc(ALLOC_SIZE);
-  if (list == NULL) return NULL;
-  list->capacity = ALLOC_SIZE;
-  list->used_capacity = ALLOC_SIZE;
-  list->start_node = NULL;
-  list->end_node = NULL;
+  list->start = NODES_MEM;
+  /*
+   * The last element of `data` will always be the last node in the list, which
+   * will be `cur_node` upon the above loop's completion.
+   */
+  list->end = cur_node;
 
   return list;
 }
 
 int main(void) {
   const int data[] = {1, 2, 3, 4, 5, 6};
-  s_linked_list *list = new_s_list(data);
+  sl_list *list = new_sl_list(data);
   return 0;
 }
--- lists/singly/singlylinkedlist.h
+++ lists/singly/singlylinkedlist.h
@@ -3,20 +3,22 @@
 
 #include <stddef.h>
 
-typedef struct singly_linked_node {
-  void *value;
-  struct singly_linked_node *next;
-} s_linked_node;
+/* singly-linked list node (sll node) */
+typedef struct sll_node {
+  size_t value_index;
+  size_t next_node_index;
+} sll_node;
 
-typedef struct singly_linked_list {
-  s_linked_node *start_node;
-  s_linked_node *end_node;
-  size_t capacity;
-  size_t used_capacity;
-} s_linked_list;
+/* singly-linked list (sl list) */
+typedef struct sl_list {
+  sll_node *start;
+  sll_node *end;
+  size_t data_allocation;
+  size_t value_size;
+} sl_list;
 
-#define new_s_list(data) \
-  _new_s_list(data, sizeof(data) / sizeof *(data), sizeof *(data))
+#define new_sl_list(data) \
+  _new_sl_list(data, sizeof(data) / sizeof *(data), sizeof *(data))
 
 /*
  * Creates a new singly linked list whose nodes contain values copied from the
@@ -25,15 +27,15 @@ typedef struct singly_linked_list {
  * \return A singly linked list whose nodes correspond to elements at `data` or
  * `NULL` upon failure.
  */
-s_linked_list *_new_s_list(const void *data, size_t num_elems,
-                           size_t elem_size);
+sl_list *_new_sl_list(const void *data, size_t num_elems, size_t elem_size);
 
-/* 
- * Appends `val` to `list` as a node, expanding `list` if necessary. 
- * 
- * \return A pointer to a singly linked list associated with the contents of `list` or `NULL` upon failure.
+/*
+ * Appends a new node containing `val` to `list`, expanding `list` if necessary.
+ *
+ * \return A pointer associated with the contents of `list` or `NULL` upon
+ * failure.
  */
-s_linked_list *add_val_as_node(s_linked_list *list, void *val);
+sl_list *sll_add_value(sl_list *list, void *val);
 
 /*
  * Appends `node` to `list`.
@@ -41,13 +43,13 @@ s_linked_list *add_val_as_node(s_linked_list *list, void *val);
  * \return A pointer to a singly linked list associated with the contents of
  * `list` or `NULL` upon failure.
  */
-s_linked_list *add_node(s_linked_list list, s_linked_node *node);
+sl_list *sll_add_node(sl_list list, sll_node *node);
 
 /*
  * Creates a new singly linked list with no nodes.
  *
  * \return An empty singly linked list or `NULL` upon failure.
  */
-s_linked_list *new_empty_s_list(size_t num_elems, size_t elem_size);
+sl_list *new_empty_sl_list(size_t num_nodes, size_t value_size);
 
 #endif
