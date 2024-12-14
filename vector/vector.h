00e3e4124013f0234e3ec222e5d7d4797bd5462e
LorenzoPalacios1 <lp434717@gmail.com>
2024-12-14 11:37:29 -0600

    refactor header file
    Signed-off-by: LorenzoPalacios1 <lp434717@gmail.com>

--- vector/vector.h
+++ vector/vector.h
@@ -5,27 +5,27 @@
 
 #define REALLOC_FACTOR (2)
 
-/* clang-format off */
-#define new_vector(data, length) _new_vector(data, sizeof*(data), length)
-#define new_vector_from_c_arr(arr) _new_vector(arr, sizeof*(arr), sizeof(arr) / sizeof*(arr))
-#define delete_vector(vec) _delete_vector(&(vec))
-#define expand_vector(vec) resize_vector(vec, (vec)->capacity * REALLOC_FACTOR)
-/* clang-format on */
+typedef unsigned char byte;
 
-typedef struct vector_t {
-  void *data;
+#define new_vector(data) \
+  _vector_new(data, sizeof *(data), sizeof(data) / sizeof *(data))
+
+typedef struct {
+  byte *data;
   size_t length;
-  size_t elem_size;
   size_t capacity;
-} vector_t;
+  size_t elem_size;
+} vector;
 
-void _delete_vector(vector_t **v);
+vector *_vector_new(const void *data, size_t elem_size, size_t length);
 
-void delete_vector_s(vector_t *v);
+void vector_delete(vector **v);
 
-vector_t *resize_vector(vector_t *vec, size_t new_size);
+void vector_delete_s(vector **v);
 
-size_t print_vector(const vector_t *vec);
+vector *vector_expand(vector *vec);
 
-vector_t *_new_vector(const void *data, size_t elem_size, size_t num_elems);
+vector *vector_init(size_t elem_size, size_t length);
+
+vector *vector_resize(vector *vec, size_t new_size);
 #endif
