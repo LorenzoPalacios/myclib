8f5c28a744a7880dcda86190848dcd76a48e2e1c
LorenzoPalacios1 <lp434717@gmail.com>
2024-12-14 16:21:01 -0600

    add for-each, revise naming scheme, general refactoring
    Signed-off-by: LorenzoPalacios1 <lp434717@gmail.com>

--- vector/vector.c
+++ vector/vector.c
@@ -4,45 +4,81 @@
 #include <stdlib.h>
 #include <string.h>
 
-vector_t *add_elem(vector_t *dest, void *elem) {
-  if (dest->length == dest->capacity) {
-    dest = expand_vector(dest);
-    if (dest == NULL) return NULL;
-  }
-  memcpy((char *)dest->data + dest->length * dest->elem_size, elem,
-         dest->elem_size);
-  dest->length++;
-  return dest;
+static inline size_t get_data_alloc(const vector *const vec) {
+  return vec->capacity * vec->elem_size;
 }
 
-inline void _delete_vector(vector_t **const vec) {
+static inline size_t get_total_alloc(const vector *const vec) {
+  return get_data_alloc(vec) + sizeof(vector);
+}
+
+vector *vector_add_elem(vector *vec, const void *const elem) {
+  if (vec->length == vec->capacity) {
+    vec = vector_expand(vec);
+    if (vec == NULL) return NULL;
+  }
+  byte *const insertion_pos = vec->data + vec->length * vec->elem_size;
+  memcpy(insertion_pos, elem, vec->elem_size);
+  vec->length++;
+  return vec;
+}
+
+void vector_delete(vector **const vec) {
   free(*vec);
   *vec = NULL;
 }
 
-inline void delete_vector_s(vector_t *vec) {
-  memset(vec, 0, vec->capacity);
-  delete_vector(vec);
+void vector_delete_s(vector **vec) {
+  memset(*vec, 0, (*vec)->capacity);
+  vector_delete(vec);
 }
 
-vector_t *resize_vector(vector_t *const vec, const size_t new_size) {
-  vector_t *new_vec = realloc(vec, new_size + sizeof(vector_t));
-  if (new_vec == NULL) return NULL;
-  new_vec->capacity = new_size;
-  new_vec->length = (new_size < new_vec->length) ? new_size : new_vec->length;
-  new_vec->data = (char *)new_vec + sizeof(vector_t);
-  return new_vec;
+void vector_for_each(vector *vec, void (*op)(void *elem)) {
+  const size_t ELEM_SIZE = vec->elem_size;
+  const size_t LEN = vec->length;
+  byte *const data = vec->data;
+  for (size_t i = 0; i < LEN; i++)
+    op(data + i * ELEM_SIZE);
 }
 
-vector_t *_new_vector(const void *const data, const size_t elem_size,
-                      const size_t length) {
+vector *vector_init(const size_t elem_size, const size_t length) {
   const size_t CAPACITY = length * elem_size;
-  vector_t *vec = malloc(CAPACITY + sizeof(vector_t));
+  vector *const vec = malloc(CAPACITY + sizeof(vector));
   if (vec == NULL) return NULL;
-  vec->data = (char *)vec + sizeof(vector_t);
+  vec->data = (byte *)(vec + 1);
+  vec->length = 0;
+  vec->capacity = length;
   vec->elem_size = elem_size;
-  vec->length = length;
+  return vec;
+}
+
+static inline size_t sanitize_new_size(const vector *const vec,
+                                       const size_t new_size) {
+  if (new_size < sizeof(vector)) return sizeof(vector);
+  const size_t ELEM_SIZE = vec->elem_size;
+  const size_t ALIGNED_SIZE = new_size - (new_size % ELEM_SIZE);
+  return ALIGNED_SIZE;
+}
+
+vector *vector_resize(vector *vec, const size_t new_size) {
+  const size_t SANITIZED_SIZE = sanitize_new_size(vec, new_size);
+  vec = realloc(vec, SANITIZED_SIZE);
+  if (vec == NULL) return NULL;
+  const size_t ELEM_SIZE = vec->elem_size;
+  const size_t ELEM_ALLOC = SANITIZED_SIZE - sizeof(vector);
+  const size_t CAPACITY = ELEM_ALLOC / ELEM_SIZE;
   vec->capacity = CAPACITY;
-  memcpy(vec->data, data, CAPACITY);
+  if (vec->length > CAPACITY) vec->length = CAPACITY;
+  vec->data = (byte*)(vec + 1);
+  return vec;
+}
+
+vector *_vector_new(const void *const data, const size_t elem_size,
+                    const size_t length) {
+  vector *vec = vector_init(elem_size, length);
+  if (vec == NULL) return NULL;
+  vec->length = length;
+  const size_t ELEM_ALLOC = length * elem_size;
+  memcpy(vec->data, data, ELEM_ALLOC);
   return vec;
 }
--- vector/vector.h
+++ vector/vector.h
@@ -7,7 +7,7 @@
 
 typedef unsigned char byte;
 
-#define new_vector(data) \
+#define vector_new(data) \
   _vector_new(data, sizeof *(data), sizeof(data) / sizeof *(data))
 
 typedef struct {
@@ -19,9 +19,13 @@ typedef struct {
 
 vector *_vector_new(const void *data, size_t elem_size, size_t length);
 
+vector *vector_add_elem(vector *vec, const void *elem);
+
 void vector_delete(vector **v);
 
 void vector_delete_s(vector **v);
+
+void vector_for_each(vector *vec, void (*op)(void *elem));
 
 vector *vector_expand(vector *vec);
 
