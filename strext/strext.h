f05f210d3d064dec8237e614e89ee644e1b37922
LorenzoPalacios1 <lp434717@gmail.com>
2024-12-22 13:11:47 -0600

    refactored header file
    Signed-off-by: LorenzoPalacios1 <lp434717@gmail.com>

--- strext/strext.h
+++ strext/strext.h
@@ -3,42 +3,40 @@
 
 #include <stdio.h>
 
-/* Capacity size in bytes. Must be greater than 0. */
-#define BASE_STR_CAPACITY (8192)
-#if (BASE_STR_CAPACITY <= 0)
-#error "BASE_STR_CAPACITY must be greater than 0."
-#endif
-
-/*
- * The factor by which functions will expand a string's allocated memory by.
- * Should be greater than 1.
- */
-#define STR_EXPANSION_FACTOR (2)
-
-typedef struct string_t {
+typedef struct {
   char *data;
   size_t length;
   size_t capacity;
-} string_t;
+  size_t allocation;
+} string;
 
-/* clang-format off */
-#define new_string(arg)                   \
-  (_Generic((arg),                        \
-  const char *: string_from_chars,        \
-  char *: string_from_chars,              \
-  FILE *: string_from_stream,             \
-  signed long long: string_of_capacity,   \
-  signed long: string_of_capacity,        \
-  signed int: string_of_capacity,         \
-  signed short: string_of_capacity,       \
-  signed char: string_of_capacity,        \
-  unsigned long long: string_of_capacity, \
-  unsigned long: string_of_capacity,      \
-  unsigned int: string_of_capacity,       \
-  unsigned short: string_of_capacity,     \
-  unsigned char: string_of_capacity       \
-  )(arg))
-/* clang-format on */
+#define new_string(arg)                          \
+  (_Generic((arg),                               \
+       char: string_of_char,                     \
+       signed char: string_of_char,              \
+       unsigned char: string_of_char,            \
+       short: string_of_capacity,                \
+       int: string_of_capacity,                  \
+       long: string_of_capacity,                 \
+       long long: string_of_capacity,            \
+       unsigned short: string_of_capacity,       \
+       unsigned int: string_of_capacity,         \
+       unsigned long: string_of_capacity,        \
+       unsigned long long: string_of_capacity,   \
+       char *: string_of_raw_str,                \
+       signed char *: string_of_raw_str,         \
+       unsigned char *: string_of_raw_str,       \
+       const char *: string_of_raw_str,          \
+       const signed char *: string_of_raw_str,   \
+       const unsigned char *: string_of_raw_str, \
+       FILE *: string_of_stream)(arg))
+
+#define string_append(appended)             \
+  (_Generic((arg),                          \
+       const char *: string_append_raw_str, \
+       char *: string_append_raw_str,       \
+       signed char: string_append_char,     \
+       unsigned char: string_append_char)(arg))
 
 #define delete_string(str) _delete_string(&(str))
 #define delete_string_s(str) _delete_string_s(&(str))
@@ -49,7 +47,7 @@ typedef struct string_t {
  * \return A pointer associated with the data of `dst`, or `NULL` if
  * the operation failed.
  */
-string_t *append_char(string_t *dst, char appended);
+string *string_append_char(string *dst, char appended);
 
 /*
  * Appends `src` to the end of `dst_str`, expanding if necessary.
@@ -57,7 +55,7 @@ string_t *append_char(string_t *dst, char appended);
  * \return A pointer associated with the data of `dst`, or `NULL` if the
  * operation failed.
  */
-string_t *append_str(string_t *dst, const string_t *src);
+string *string_append_str(string *dst, string *src);
 
 /*
  * Appends `src` to the end of `dst_str`, expanding if necessary.
@@ -65,122 +63,129 @@ string_t *append_str(string_t *dst, const string_t *src);
  * \return A pointer associated with the data of `dst`, or `NULL` if the
  * operation failed.
  */
-string_t *append_raw_str(string_t *dst, const char *src, size_t src_len);
+string *append_raw_str(string *dst, const char *src, size_t src_len);
 
-void _delete_string(string_t **str_obj);
+void _string_delete(string **str_obj);
 
-void _delete_string_s(string_t **str_obj);
+void _string_delete_s(string **str_obj);
 
-string_t *erase_string_contents(string_t *const str);
+string *string_clear(string *str);
+
+string *string_clear_s(string *str);
 
 /*
- * Expands the passed string's allocated boundaries by `expansion_factor`,
- * updating the stats of `str_obj` as necessary.
+ * Expands the passed string's allocated boundaries by
+ * `expansion_factor`, updating the stats of `str_obj` as necessary.
  *
- * \return A (possibly new) pointer associated with the data of `str_obj`,
- * or `NULL` if reallocation failed.
+ * \return A (possibly new) pointer associated with the data of
+ * `str_obj`, or `NULL` if reallocation failed.
  *
  * \note If reallocation failed, `str_obj` will be unmodified.
  */
-string_t *expand_string(string_t *str_obj);
+string *string_expand(string *str_obj);
 
 /*
- * Finds the first occurrence of `needle` within `haystack` starting from the
- * beginning of `haystack->data` and replaces it with `replacement`, expanding
- * `haystack` as necessary.
+ * Finds the first occurrence of `needle` within `haystack` starting
+ * from the beginning of `haystack->data` and replaces it with
+ * `replacement`, expanding `haystack` as necessary.
  *
- * \return A (possibly new) pointer associated with the data of `haystack`, or
- * `NULL` if the operation failed.
+ * \return A (possibly new) pointer associated with the data of
+ * `haystack`, or `NULL` if the operation failed.
  *
- * \note A returned `NULL` pointer does not guarantee `haystack` is unmodified
- * by this function.
+ * \note A returned `NULL` pointer does not guarantee `haystack` is
+ * unmodified by this function.
  */
-string_t *find_replace(string_t *haystack, const string_t *needle,
-                       const string_t *replacer);
+string *string_find_replace(string *hay, const string *needle,
+                            const string *replace);
 
 /*
  * Finds all occurrences of `needle` within `haystack` starting from the
- * beginning of `haystack->data`, replacing any findings with `replacement` and
- * expanding `haystack` as necessary.
+ * beginning of `haystack->data`, replacing any findings with
+ * `replacement` and expanding `haystack` as necessary.
  *
- * \return A (possibly new) pointer associated with the data of `haystack`, or
- * `NULL` if the operation failed.
+ * \return A (possibly new) pointer associated with the data of
+ * `haystack`, or `NULL` if the operation failed.
  *
- * \note A returned `NULL` pointer does not guarantee `haystack` is unmodified
- * by this function.
+ * \note A returned `NULL` pointer does not guarantee `haystack` is
+ * unmodified by this function.
  *
  * \attention Function not yet complete.
  */
-string_t *find_replace_all(string_t *haystack, const string_t *needle,
-                           const string_t *replacement);
+string *string_find_replace_all(string *hay, const string *needle,
+                                const string *replace);
 
 /*
- * Reallocates the memory used for `str_obj` to fit `new_size` bytes, updating
- * the stats of `str_obj` as necessary.
+ * Reallocates the memory used for `str_obj` to fit `new_size` bytes,
+ * updating the stats of `str_obj` as necessary.
  *
- * This function only modifies the memory allocated for characters, meaning
- * there will always be enough space for the data members of `string_t`
- * regardless of the value passed as `new_size`.
+ * This function only modifies the memory allocated for characters,
+ * meaning there will always be enough space for the data members of
+ * `string` regardless of the value passed as `new_size`.
  *
- * \return A (possibly new) pointer associated with the data of `str_obj`, or
- * `NULL` if reallocation failed.
+ * \return A (possibly new) pointer associated with the data of
+ * `str_obj`, or `NULL` if reallocation failed.
  *
  * \note If reallocation failed, `str_obj` will be unmodified.
  */
-string_t *resize_string(string_t *str_obj, size_t new_size);
+string *string_resize(string *str_obj, size_t new_size);
 
 /*
- * Shrinks the memory used for `str_obj` to fit the number of characters it
- * contains.
+ * Shrinks the memory used for `str_obj` to fit the number of characters
+ * it contains.
  *
- * \return A (possibly new) pointer associated with the data of `str_obj`, or
- * `NULL` if reallocation failed.
+ * \return A (possibly new) pointer associated with the data of
+ * `str_obj`, or `NULL` if reallocation failed.
  */
-string_t *shrink_alloc_to_length(string_t *str_obj);
+string *string_shrink_alloc(string *str_obj);
+
+string *string_new(void);
+
+string *string_of_char(char chr);
 
 /*
- * Generates a `string_t` object whose `data` consists of the passed raw
- * null-terminated string, `raw_text`.
+ * Generates a `string` object whose `data` consists of the passed raw
+ * null-terminated string, `raw_str`.
  *
- * \return A `string_t` object containing characters from `raw_text`, or `NULL`
- * upon failure.
+ * \return A `string` object containing characters from `raw_str`, or
+ * `NULL` upon failure.
  */
-string_t *string_from_chars(const char *raw_text);
+string *string_of_raw_str(const char *raw_str);
 
 /*
- * Generates a `string_t` object whose `data` consists of a single line of
+ * Generates a `string` object whose `data` consists of a single line of
  * characters from `stdin`.
  *
- * \return A `string_t` object containing characters from `stdin`, or `NULL`
- * upon failure.
+ * \return A `string` object containing characters from `stdin`, or
+ * `NULL` upon failure.
  */
-string_t *string_from_line_stdin(void);
+string *string_of_line_stdin(void);
 
 /*
- * Creates a `string_t` object whose `data` consists of characters within
+ * Creates a `string` object whose `data` consists of characters within
  * `stream` until `EOF` is met.
  *
- * \return A pointer to a `string_t` object containing characters from `stream`,
- * or `NULL` upon failure.
+ * \return A pointer to a `string` object containing characters from
+ * `stream`, or `NULL` upon failure.
  */
-string_t *string_from_stream(FILE *stream);
+string *string_of_stream(FILE *stream);
 
 /*
- * Creates a `string_t` object whose `data` consists of characters from `stream`
- * until either `EOF` or the specified `delim` character is met.
+ * Creates a `string` object whose `data` consists of characters from
+ * `stream` until either `EOF` or the specified `delim` character is
+ * met.
  *
- * \return A pointer to a `string_t` object containing characters from `stream`,
- * or `NULL` upon failure.
+ * \return A pointer to a `string` object containing characters from
+ * `stream`, or `NULL` upon failure.
  */
-string_t *string_from_stream_given_delim(FILE *stream, char delim);
+string *string_of_stream_delim(FILE *stream, char delim);
 
 /*
- * Creates a `string_t` object capable of storing `capacity` number of
+ * Creates a `string` object capable of storing `capacity` number of
  * characters.
  *
- * \return A pointer to a `string_t` object of size
- * `capacity + sizeof(string_t)`, or `NULL` upon failure.
+ * \return A pointer to a `string` object of size
+ * `capacity + sizeof(string)`, or `NULL` upon failure.
  */
-string_t *string_of_capacity(const size_t capacity);
+string *string_of_capacity(const size_t capacity);
 
 #endif
