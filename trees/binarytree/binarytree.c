bdbe4f040d911439773579f7a16117b82a2e3f2d
LorenzoPalacios <lp902470@gmail.com>
2025-02-05 20:35:48 -0600

    update binary tree

--- trees/binarytree/binarytree.c
+++ trees/binarytree/binarytree.c
@@ -1,62 +1,388 @@
 #include "binarytree.h"
 
+#include <stdalign.h>
+#include <stdbool.h>
 #include <stddef.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include "../trees.h"
+typedef unsigned char byte;
 
-binary_tree *_new_binary_tree(const void *const data, const size_t elem_size,
-                              const size_t length) {
-  const size_t NODE_SIZE = sizeof(bt_node) + elem_size;
-  const size_t REQUIRED_MEM = length * NODE_SIZE + sizeof(binary_tree);
-  binary_tree *const tree_obj = malloc(REQUIRED_MEM);
-  /* Increment past the tree's data. */
-  bt_node *const nodes_mem = (void *)(tree_obj + 1);
-  if (tree_obj == NULL) return NULL;
+typedef struct bt_node {
+  size_t parent_index;
+  size_t left_index;
+  size_t right_index;
+} bt_node;
 
-  tree_obj->num_nodes = length;
-  tree_obj->node_size = NODE_SIZE;
-  tree_obj->allocation = REQUIRED_MEM;
+/**
+ * `deleted_nodes` is a `stack` containing indicies to deleted nodes
+ * (relative to `get_nodes()`). `deleted_nodes` will never contain `NULL_INDEX`,
+ * but will return a `NULL` pointer to an index when empty.
+ *
+ * `allocation` describes the total amount of bytes allocated for the tree.
+ *
+ * `root_index` describes the index of the root node
+ * (relative to `get_nodes()`).
+ *
+ * `value_size` describes the size of a value.
+ *
+ * `unallocated_region_index` is the current index of the unallocated region
+ * within the nodes segment (distinct from deleted nodes).
+ */
+typedef struct binary_tree {
+  stack *deleted_nodes;
+  size_t allocation;  // Total bytes allocated for the tree and nodes.
+  size_t root_index;
+  size_t value_size;  // Size (in bytes) of a stored value.
+  size_t unallocated_region_index;
+} binary_tree;
 
-  for (size_t i = 0; i < length; i++) {
-    bt_node *const cur_node = (void *)((char *)nodes_mem + i * NODE_SIZE);
-    bt_node *parent_node;
-    if (i == 0)
-      parent_node = NULL;
-    else {
-      parent_node = (void *)((char *)cur_node - NODE_SIZE * (i / 2 + 1));
-      if (i & 1)
-        parent_node->left = cur_node;
-      else
-        parent_node->right = cur_node;
-    }
-    void *const cur_node_data = (char *)cur_node + sizeof(bt_node);
-    memcpy(cur_node_data, (char *)data + i * elem_size, elem_size);
-    cur_node->value = cur_node_data;
-    cur_node->parent = parent_node;
-    cur_node->left = cur_node->right = NULL;
+#include "../../stack/stack.h"
+
+// Reallocation factor used by `bt_expand()`.
+#define EXPANSION_FACTOR (2)
+
+// - GETTERS BEGIN -
+
+// Miscellaneous getters.
+
+static inline size_t usable_allocation(const binary_tree *const tree) {
+  return tree->allocation - sizeof(binary_tree);
+}
+
+// An individual node's true allocation is counted as the size of its value and
+// the size of a `bt_node`.
+static inline size_t node_value_pair_size(const binary_tree *const tree) {
+  return tree->value_size + sizeof(bt_node);
+}
+
+// This includes deleted and unallocated nodes.
+static inline size_t cnt_nodes(const binary_tree *const tree) {
+  return usable_allocation(tree) / node_value_pair_size(tree);
+}
+
+static inline size_t cnt_deleted_nodes(const binary_tree *const tree) {
+  return tree->deleted_nodes->length;
+}
+
+static inline size_t cnt_unallocated_nodes(const binary_tree *const tree) {
+  return cnt_nodes(tree) - tree->unallocated_region_index;
+}
+
+static inline size_t cnt_active_nodes(const binary_tree *const tree) {
+  return cnt_nodes(tree) - cnt_deleted_nodes(tree) -
+         cnt_unallocated_nodes(tree);
+}
+
+// Getters for specific extents of allocations.
+
+static inline size_t calc_padding(const size_t values_alloc) {
+  if (values_alloc == 0) return 0;
+  return alignof(bt_node) - (values_alloc % alignof(bt_node));
+}
+
+static inline size_t values_allocation(const binary_tree *const tree) {
+  return cnt_nodes(tree) * tree->value_size;
+}
+
+static inline size_t get_padding(const binary_tree *const tree) {
+  return calc_padding(values_allocation(tree));
+}
+
+// Getters for specific regions of memory.
+
+static inline byte *get_values(binary_tree *const tree) {
+  return (byte *)(tree + 1);
+}
+
+static inline bt_node *get_nodes(binary_tree *const tree) {
+  /*
+   * Although `bt_node` has a greater alignment requirement than `byte` (the
+   * type returned by `get_values()`), the casted value is guaranteed to be
+   * aligned to the size of `bt_node` with the padding bytes. The cast to `void
+   * *` is present to remove the associated linter warning.
+   */
+  return (void *)(get_values(tree) + values_allocation(tree) +
+                  get_padding(tree));
+}
+
+// Getters for specific nodes and values.
+
+static inline bt_node *node_from_index(binary_tree *const tree,
+                                           const size_t index) {
+  if (index == NULL_INDEX) return NULL;
+  return get_nodes(tree) + index;
+}
+
+static inline void *value_from_index(binary_tree *const tree,
+                                         const size_t index) {
+  if (index == NULL_INDEX) return NULL;
+  return get_values(tree) + (index * tree->value_size);
+}
+
+// Getter for indicies of nodes.
+
+static inline size_t get_node_index(binary_tree *const tree,
+                                    const bt_node *const node) {
+  if (node == NULL) return NULL_INDEX;
+  return (size_t)(node - get_nodes(tree));
+}
+
+// Getter for node values
+
+static inline void *get_node_value(binary_tree *const tree,
+                                   bt_node *const node) {
+  if (node == NULL) return NULL;
+  return value_from_index(tree, get_node_index(tree, node));
+}
+
+// Getters for nodes based on relative positions.
+
+static inline bt_node *root_node(binary_tree *const tree) {
+  if (tree->root_index == NULL_INDEX) return NULL;
+  return node_from_index(tree, tree->root_index);
+}
+
+static inline bt_node *parent_node(binary_tree *const tree,
+                                       const bt_node *const node) {
+  if (node->parent_index == NULL_INDEX) return NULL;
+  return node_from_index(tree, node->parent_index);
+}
+
+static inline bt_node *left_node(binary_tree *const tree,
+                                     const bt_node *const node) {
+  if (node->left_index == NULL_INDEX) return NULL;
+  return node_from_index(tree, node->left_index);
+}
+
+static inline bt_node *right_node(binary_tree *const tree,
+                                      const bt_node *const node) {
+  if (node->right_index == NULL_INDEX) return NULL;
+  return node_from_index(tree, node->right_index);
+}
+
+// - GETTERS END -
+
+// - HELPER FUNCTIONS -
+
+// Helper function used by `get_unalloc_node()`.
+static inline bt_node *get_deleted_node(binary_tree *const tree) {
+  const size_t *const node_index = (size_t *)stack_pop(tree->deleted_nodes);
+  return (node_index == NULL) ? NULL : node_from_index(tree, *node_index);
+}
+
+// Helper function used by `bt_add_node()`.
+static inline size_t leftmost_leaf_index(binary_tree *const tree) {
+  const bt_node *candidate = root_node(tree);
+  if (candidate != NULL) {
+    while (candidate->left_index != NULL_INDEX)
+      candidate = left_node(tree, candidate);
+    return get_node_index(tree, candidate);
   }
-  tree_obj->root = nodes_mem; /* The first node is always the root node. */
-  return tree_obj;
+  return NULL_INDEX;
 }
 
-void delete_binary_tree(binary_tree **const tree) {
-  free(*tree);
-  *tree = NULL;
+// Helper function used by `bt_add_node()`.
+static inline bt_node *get_unalloc_node(binary_tree *tree) {
+  const size_t NODE_CNT = cnt_nodes(tree);
+  if (cnt_nodes(tree) == cnt_active_nodes(tree)) return NULL;
+  if (cnt_deleted_nodes(tree) != 0) return get_deleted_node(tree);
+  if (tree->unallocated_region_index == NODE_CNT) return NULL;
+  return get_nodes(tree) + (tree->unallocated_region_index++);
 }
 
-void delete_binary_tree_s(binary_tree **const tree) {
-  memset(*tree, 0, (*tree)->allocation);
-  delete_binary_tree(tree);
+// Used by `bt_delete_node()` in tandem with `bt_traverse()`.
+static inline bool register_deleted_node(binary_tree *const tree,
+                                  bt_node *const node) {
+  const size_t NODE_INDEX = get_node_index(tree, node);
+  tree->deleted_nodes = stack_push(tree->deleted_nodes, &NODE_INDEX);
+  return true;
 }
 
-bt_node *unparent_node(binary_tree *const src, bt_node *const target);
+// - HELPER FUNCTIONS END -
 
-int main(void) {
-  static const int data[] = {1, 2, 3, 4, 5, 6, 7};
-  binary_tree *a = new_binary_tree(data, sizeof data / sizeof *data);
+binary_tree *bt_add_node(binary_tree *tree, const void *const value) {
+  if (cnt_nodes(tree) == cnt_active_nodes(tree)) {
+    tree = bt_expand(tree);
+    if (tree == NULL) return NULL;
+  }
 
-  return 0;
+  bt_node *const node = get_unalloc_node(tree);
+  memcpy(get_node_value(tree, node), value, tree->value_size);
+
+  const size_t PARENT_INDEX = leftmost_leaf_index(tree);
+  const size_t NODE_INDEX = get_node_index(tree, node);
+  if (PARENT_INDEX != NULL_INDEX) {
+    bt_node *const parent = node_from_index(tree, PARENT_INDEX);
+    parent->left_index = NODE_INDEX;
+    node->parent_index = PARENT_INDEX;
+    node->left_index = node->right_index = NULL_INDEX;
+  } else {
+    tree->root_index = NODE_INDEX;
+    node->parent_index = node->left_index = node->right_index = NULL_INDEX;
+  }
+  return tree;
+}
+
+inline void bt_delete(binary_tree *const tree) {
+  if (tree->deleted_nodes != NULL) stack_delete(tree->deleted_nodes);
+  free(tree);
+}
+
+void bt_delete_node(binary_tree *tree, bt_node *const node) {
+  bt_node *const parent = parent_node(tree, node);
+  if (parent != NULL) {
+    if (parent->left_index == get_node_index(tree, node))
+      parent->left_index = NULL_INDEX;
+    else
+      parent->right_index = NULL_INDEX;
+  } else {
+    tree->root_index = NULL_INDEX;
+  }
+  bt_traverse(tree, node, register_deleted_node);
+}
+
+binary_tree *bt_expand(binary_tree *const tree) {
+  const size_t NUM_NODES = cnt_nodes(tree);
+  if (NUM_NODES == 0) return bt_resize(tree, 1);
+  return bt_resize(tree, EXPANSION_FACTOR * NUM_NODES);
+}
+
+binary_tree *bt_init(const size_t value_size, const size_t node_cnt) {
+  const size_t NODES_ALLOC = sizeof(bt_node) * node_cnt;
+  const size_t VALUES_ALLOC = value_size * node_cnt;
+  const size_t PADDING = calc_padding(VALUES_ALLOC);
+  const size_t TOTAL_ALLOC =
+      NODES_ALLOC + VALUES_ALLOC + PADDING + sizeof(binary_tree);
+  binary_tree *const tree = malloc(TOTAL_ALLOC);
+  if (tree == NULL) return NULL;
+  tree->deleted_nodes = stack_empty_new(node_cnt / 4, sizeof(size_t));
+  tree->allocation = TOTAL_ALLOC;
+  tree->root_index = NULL_INDEX;
+  tree->value_size = value_size;
+  tree->unallocated_region_index = 0;
+  return tree;
+}
+
+binary_tree *bt_new_(const void *const data, const size_t value_size,
+                     const size_t num_values) {
+  binary_tree *const tree = bt_init(value_size, num_values);
+  if (tree == NULL) return NULL;
+  tree->unallocated_region_index = num_values;
+
+  bt_node *const nodes = get_nodes(tree);
+  if (num_values > 0) {
+    tree->root_index = 0;
+    bt_node *const root_node = nodes;
+    root_node->parent_index = NULL_INDEX;
+    root_node->left_index = NULL_INDEX;
+    root_node->right_index = NULL_INDEX;
+  }
+  for (size_t i = 1; i < num_values; i++) {
+    const size_t PARENT_INDEX = (i - 1) / 2;
+    bt_node *const node = nodes + i;
+    bt_node *const parent = nodes + PARENT_INDEX;
+    node->parent_index = PARENT_INDEX;
+    // Each new node should start without children.
+    node->left_index = node->right_index = NULL_INDEX;
+    /*
+     * The line below is equivalent to:
+     *  if (i & 1)
+     *    parent->left_index = i;
+     *  else
+     *    parent->right_index = i;
+     */
+    *((size_t *)parent + 1 + !(i & 1)) = i;
+  }
+  memcpy(get_values(tree), data, value_size * num_values);
+  return tree;
+}
+
+binary_tree *bt_resize(binary_tree *tree, const size_t new_capacity) {
+  if (new_capacity < tree->unallocated_region_index) return tree;
+  const size_t PAIR_SIZE = node_value_pair_size(tree);
+  const size_t PADDING = calc_padding(tree->value_size * new_capacity);
+  const size_t NEW_ALLOCATION =
+      (PAIR_SIZE * new_capacity) + sizeof(binary_tree) + PADDING;
+  {
+    if (NEW_ALLOCATION == tree->allocation) return tree;
+    binary_tree *const new_tree = realloc(tree, NEW_ALLOCATION);
+    if (new_tree == NULL) return NULL;
+    tree = new_tree;
+  }
+  if (new_capacity == 0) {
+    tree->root_index = NULL_INDEX;
+    tree->allocation = NEW_ALLOCATION;
+    stack_reset(tree->deleted_nodes);
+  } else {
+    bt_node *const old_nodes_region = get_nodes(tree);
+    tree->allocation = NEW_ALLOCATION;
+    bt_node *const new_nodes_region = get_nodes(tree);
+    const size_t MOVE_SIZE = sizeof(bt_node) * cnt_nodes(tree);
+    memmove(new_nodes_region, old_nodes_region, MOVE_SIZE);
+  }
+  return tree;
+}
+
+void bt_swap_values(binary_tree *const tree, bt_node *const node_1,
+                    bt_node *const node_2) {
+  if (node_1 == node_2) return;
+  const size_t VALUE_SIZE = tree->value_size;
+  byte *restrict const value_1 = get_node_value(tree, node_1);
+  byte *restrict const value_2 = get_node_value(tree, node_2);
+  for (size_t i = 0; i < VALUE_SIZE; i++) {
+    const byte temp = value_1[i];
+    value_1[i] = value_2[i];
+    value_2[i] = temp;
+  }
+}
+
+/*
+ * `bt_traverse()` will, by default, traverse first along the left branch,
+ * deviating to a right branch if and only if the `left_index` of a traversed
+ * node is `NULL_INDEX`. If both `left_index` and `right_index` are
+ * `NULL_INDEX`, then `bt_traverse()` backtracks to the closest ancestor
+ * node with a valid right branch that has not yet been traversed.
+ *
+ * Recursion is the natural solution to tree traversal, but recursion on a
+ * particularly large tree could result in a stack overflow. Hence, we use a
+ * stack (allocated on the heap) to keep track of divergent nodes in the tree.
+ */
+void bt_traverse(binary_tree *const tree, bt_node *const from,
+                 bool (*const operation)(binary_tree *tree, bt_node *node)) {
+  stack *divergent_nodes =
+      stack_empty_new(cnt_active_nodes(tree), sizeof(bt_node **));
+
+  bt_node *cur_node = from;
+  bt_node *next_node = NULL;
+  while (cur_node != NULL) {
+    if (operation != NULL)
+      if (!operation(tree, cur_node)) break;
+
+    bt_node *const left = left_node(tree, cur_node);
+    bt_node *const right = right_node(tree, cur_node);
+    if (left != NULL) {
+      /*
+       * If both `left` and `right` are valid, continue down the left branch
+       * and save `node` as a divergent node.
+       *
+       * Taking the address of `node` is necessary since `stack_push()`
+       * expects the `elem` argument to be a pointer to data. Since `node` is
+       * a pointer to a node, and we need to keep track of that pointer, we
+       * take the address of that pointer.
+       */
+      if (right != NULL)
+        divergent_nodes = stack_push(divergent_nodes, (void *)&cur_node);
+      next_node = left;
+    } else if (right != NULL) {
+      next_node = right;
+    } else {
+      bt_node **const next_divergence = (bt_node **)stack_pop(divergent_nodes);
+      // If there are no divergent nodes in the traversed path, we're done.
+      if (next_divergence == NULL) break;
+      next_node = right_node(tree, *next_divergence);
+    }
+    cur_node = next_node;
+  }
+  stack_delete(divergent_nodes);
 }
--- trees/binarytree/binarytree.h
+++ trees/binarytree/binarytree.h
@@ -1,78 +1,150 @@
 #ifndef BINARYTREE_H
 #define BINARYTREE_H
 
+#include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
 
-#include "../trees.h"
+// Stacks are used to track unallocated nodes in trees and in
+// `bt_traverse()` to avoid the usage of recursion.
+#include "../../stack/stack.h"
 
-typedef struct bt_node {
-  void *value;
-  struct bt_node *parent;
-  struct bt_node *left, *right;
-} bt_node;
+#define NULL_INDEX (SIZE_MAX)
 
-/* Container structure for a binary tree data structure. */
-typedef struct {
-  bt_node *root;
-  size_t num_nodes;
-  size_t node_size;
-  size_t allocation; /* Total bytes allocated for the tree and nodes. */
-} binary_tree;
+// Binary tree node.
+typedef struct bt_node bt_node;
 
-/*
- * This is a convenience macro for generating a `binary_tree` from an array.
- * Make sure the arguments have no side effects such as incrementation.
+// Binary tree header.
+typedef struct binary_tree binary_tree;
+
+/**
+ * @brief Macro to create a new binary tree.
+ *
+ * This macro initializes a new binary tree with the given data.
+ *
+ * @param data The data to initialize the binary tree with.
  */
-#define new_binary_tree(data, length) \
-  _new_binary_tree(data, sizeof *(data), length)
+#define bt_new(data) bt_new_(data, sizeof *(data), sizeof(data) / sizeof *(data))
 
-/*
- * Initializes a binary tree with the given elements from the passed array.
- * The first element in the array is always the root node, and subsequent
+/**
+ * @brief Macro to traverse a binary tree.
+ *
+ * This macro traverses the binary tree starting from the root node and applies the given operation.
+ *
+ * @param tree The binary tree to traverse.
+ * @param op The operation to apply to each node during traversal.
+ */
+#define bt_traverse_tree(tree, op) bt_traverse(tree, get_root_node(tree), op)
+
+/**
+ * @brief Adds a new node with the given value to the binary tree.
+ *
+ * @param tree A pointer to the binary tree.
+ * @param value A pointer to the value to be added.
+ * @return A pointer to the updated binary tree, or `NULL` if the operation
+ * fails.
+ * @note The added value must be the same size as `tree->value_size`.
+ */
+binary_tree *bt_add_node(binary_tree *tree, const void *value);
+
+/**
+ * @brief Deletes the binary tree and frees its memory.
+ *
+ * @param tree A pointer to the binary tree to be deleted.
+ */
+void bt_delete(binary_tree *tree);
+
+/**
+ * @brief Deletes a specific node and its descendants from the binary tree.
+ *
+ * @param tree A pointer to the binary tree.
+ * @param node A pointer to the node to be deleted.
+ */
+void bt_delete_node(binary_tree *tree, bt_node *node);
+
+/**
+ * @brief Expands the binary tree to accommodate more nodes.
+ *
+ * @param tree A pointer to the binary tree.
+ * @return A pointer to the expanded binary tree, or NULL if the operation
+ * fails.
+ */
+binary_tree *bt_expand(binary_tree *tree);
+
+/**
+ * @brief Initializes a binary tree with a specified number of nodes.
+ *
+ * @param value_size The size of each value in bytes.
+ * @param node_cnt The number of nodes to allocate.
+ * @return A pointer to the initialized binary tree, or NULL if the operation
+ * fails.
+ */
+binary_tree *bt_init(size_t value_size, size_t node_cnt);
+
+/**
+ * @brief Initializes a binary tree with the given elements from the passed
+ * array.
+ *
+ * The first element in the array is always the root node and subsequent
  * array elements fill into each node's `left` and `right` consecutively
  * starting from the left.
  *
  * Ex. Consider an array of seven elements: {7, 4, 9, 3, 0, 1, 0}.
- *     The resultant tree would look like this:
+ *     The resultant binary tree would look like this:
  *
  *         7
  *       /   \
  *      4     9
  *     / \   / \
  *    3   0 1   0
- */
-binary_tree *_new_binary_tree(const void *data, size_t elem_size,
-                              size_t length);
-
-/*
- * Frees the passed binary tree's consumed memory and reassigns its pointer
- * to `NULL`.
- */
-void delete_binary_tree(binary_tree **tree);
-
-/*
- * Same as `delete_binary_tree()`, except this function will set all allocated
- * memory of the tree to zero, including pointers and tree/node statistics.
- */
-void delete_binary_tree_s(binary_tree **tree);
-
-/*
- * Adds the specified element as a child node of `parent_node`.
- * \return A pointer to the added node, or NULL upon failure.
- */
-bt_node *add_binary_node(binary_tree *tree, const void *elem);
-
-/*
- * Removes `target` from `src`, thereby causing `target` to have no parent
- * (assumed `NULL`).
  *
- * \return The removed node.
+ * @param data A pointer to the array of values.
+ * @param value_size The size of each value in bytes.
+ * @param num_values The number of values in the array.
+ * @return A pointer to the initialized binary tree, or NULL if the operation
+ * fails.
  */
-bt_node *remove_binary_node(binary_tree *src, bt_node *target);
+binary_tree *bt_new_(const void *data, size_t value_size, size_t num_values);
 
-/*
- * This function makes `child` a child node of `parent`, including any child
- * nodes.
+/**
+ * @brief Resizes the memory allocated for the binary tree to the new capacity.
+ *
+ * @param tree A pointer to the binary tree.
+ * @param new_capacity The new capacity for the binary tree.
+ * @return A pointer to the resized binary tree, or NULL if the operation fails.
+ * @note If `new_capacity` would cause active nodes to be lost, the operation
+ * will fail.
  */
-void reparent_binary_node(const bt_node *parent, const bt_node *child);
+binary_tree *bt_resize(binary_tree *tree, size_t new_capacity);
+
+/**
+ * @brief Swaps the values of two nodes in a binary tree.
+ *
+ * This function takes a binary tree and two nodes within that tree, and swaps
+ * their values. The structure of the tree remains unchanged.
+ *
+ * @param tree A pointer to the binary tree.
+ * @param node_1 A pointer to the first node whose value is to be swapped.
+ * @param node_2 A pointer to the second node whose value is to be swapped.
+ */
+void bt_swap_values(binary_tree *tree, bt_node *node_1, bt_node *node_2);
+
+/**
+ * @brief Traverses all of the descendant nodes of `origin` and `origin`.
+ *
+ * This function passes `tree` and each traversed node to `operation` until
+ * `operation` returns `false` or all the nodes in the lineage of `origin`
+ * have been traversed.
+ *
+ * This function is NOT recursive; it has a dependency on the `stack` data
+ * structure implemented elsewhere in this library.
+ *
+ * @param tree A pointer to the binary tree.
+ * @param from A pointer to the starting node for traversal.
+ * @param operation A function pointer to the operation to be performed on each
+ * node.
+ */
+void bt_traverse(binary_tree *tree, bt_node *from,
+                 bool (*operation)(binary_tree *tree, bt_node *node));
+
 #endif
