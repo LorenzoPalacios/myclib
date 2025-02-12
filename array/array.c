859d17e8e4866730e2f04b240ad578f966c118b9
LorenzoPalacios <lp902470@gmail.com>
2025-02-11 18:54:44 -0600

    array: refactored constructors to return values
    Signed-off-by: LorenzoPalacios <lp902470@gmail.com>

--- array/array.c
+++ array/array.c
@@ -6,65 +6,57 @@
 
 typedef unsigned char byte;
 
-array *array_new_(const void *const data, const size_t elem_size,
-                  const size_t length) {
-  array *const new_arr = array_init(elem_size, length);
-  if (new_arr == NULL) return NULL;
-  memcpy(new_arr->data, data, elem_size * length);
-  new_arr->length = length;
-  return new_arr;
+static inline void *internal_array_get(array *const arr, const size_t index) {
+  return (byte *)arr->data + (index * arr->elem_size);
 }
 
-void *array_add(array *const arr, const void *const elem) {
-  if (arr->length == arr->capacity) return NULL;
-  void *const elem_pos = (byte *)arr->data + arr->length;
+array array_new_(const void *const data, const size_t elem_size,
+                 const size_t length) {
+  array arr = array_init(elem_size, length);
+  memcpy(arr.data, data, elem_size * length);
+  return arr;
+}
+
+void *array_set_(array *const arr, const void *const elem, const size_t index) {
+  if (index >= arr->length) return NULL;
+  void *const elem_pos = internal_array_get(arr, index);
   memcpy(elem_pos, elem, arr->elem_size);
-  arr->length++;
   return elem_pos;
 }
 
-void array_for_each(array *const arr, void (*const operation)(void *elem)) {
+void array_for_each_(array *const arr, void (*const operation)(void *elem)) {
   const size_t LEN = arr->length;
-  for (size_t i = 0; i < LEN; i++) operation(array_get(arr, i));
+  for (size_t i = 0; i < LEN; i++) operation(array_get_(arr, i));
 }
 
-array *array_init(const size_t elem_size, const size_t length) {
-  const size_t ELEM_ALLOC = elem_size * length;
-  array *const new_arr = malloc(ELEM_ALLOC + sizeof(array));
-  if (new_arr == NULL) return NULL;
-  new_arr->data = (byte *)(new_arr + 1);
-  new_arr->capacity = length;
-  new_arr->length = 0;
-  new_arr->elem_size = elem_size;
-  return new_arr;
+array array_init(const size_t elem_size, const size_t length) {
+  void *const arr_contents = malloc(elem_size * length);
+  const array arr = {
+      .data = arr_contents, .length = length, .elem_size = elem_size};
+  return arr;
 }
 
-void array_delete(array *const arr) {
-  free(arr);
+void array_delete_(array *const arr) {
+  free(arr->data);
 }
 
-void *array_get(const array *const arr, const size_t index) {
+void *array_get_(const array *const arr, const size_t index) {
   if (index >= arr->length) return NULL;
   return (byte *)arr->data + (arr->elem_size * index);
 }
 
-void *array_insert(array *const arr, const void *const elem, size_t index) {
-  if (arr->length == arr->capacity) return NULL;
-  if (index > arr->length) return NULL;
+void *array_insert_(array *const arr, const void *const elem, size_t index) {
+  if (index >= arr->length) return NULL;
   const size_t ELEM_SIZE = arr->elem_size;
-
-  byte *const elem_pos = array_get(arr, index);
+  byte *const elem_pos = array_get_(arr, index);
   byte *const after_elem_pos = elem_pos + ELEM_SIZE;
   const size_t REMAINDER_LENGTH = arr->length - index;
   memmove(after_elem_pos, elem_pos, ELEM_SIZE * REMAINDER_LENGTH);
   memcpy(elem_pos, elem, arr->elem_size);
-
-  arr->length++;
   return elem_pos;
 }
 
-void array_clear(array *const arr) {
-  const size_t ELEM_ALLOC = arr->elem_size * arr->capacity;
+void array_clear_(array *const arr) {
+  const size_t ELEM_ALLOC = arr->elem_size * arr->length;
   memset(arr->data, 0, ELEM_ALLOC);
-  arr->length = 0;
 }
--- array/array.h
+++ array/array.h
@@ -6,52 +6,54 @@
 #define array_new(data) \
   array_new_(data, sizeof *(data), sizeof(data) / sizeof *(data))
 
+#define array_set(arr, elem) array_add_(&(arr), elem)
+
+#define array_clear(arr) array_clear_(&(arr))
+
+#define array_delete(arr) array_delete_(&(arr))
+
+#define array_for_each(arr, op) array_for_each_(&(arr), op)
+
+#define array_get(arr, index) array_get_(&(arr), index)
+
+#define array_insert(arr, elem) array_insert_(&(arr), elem)
+
 typedef struct {
-  void *data;
-  size_t capacity;
-  size_t length;
-  size_t elem_size;
+  void *const data;
+  const size_t length;
+  const size_t elem_size;
 } array;
 
 /**
- * @brief Adds an element to the array.
- *
- * @param arr The array to add the element to.
- * @param elem The element to add.
- * @return A pointer to the added element, or NULL if the array is full.
- */
-void *array_add(array *arr, const void *elem);
-
-/**
- * @brief Clears the array, setting all elements to zero.
+ * @brief Clears an array, setting all elements to zero.
  *
  * @param arr The array to clear.
  */
-void array_clear(array *arr);
+void array_clear_(array *arr);
 
 /**
- * @brief Deletes the array and frees its memory.
+ * @brief Deletes an array and frees its memory.
  *
  * @param arr A pointer to the array to delete.
  */
-void array_delete(array *arr);
+void array_delete_(array *arr);
 
 /**
- * @brief Applies a function to each element of the array.
+ * @brief Applies a function to each element of an array.
  *
  * @param arr The array to iterate over.
  * @param operation The function to apply to each element.
  */
-void array_for_each(array *arr, void (*operation)(void *elem));
+void array_for_each_(array *arr, void (*operation)(void *elem));
 
 /**
- * @brief Gets an element from the array by index.
+ * @brief Gets an element from an array by index.
  *
  * @param arr The array to get the element from.
  * @param index The index of the element to get.
  * @return A pointer to the element, or NULL if the index is out of bounds.
  */
-void *array_get(const array *arr, size_t index);
+void *array_get_(const array *arr, size_t index);
 
 /**
  * @brief Initializes a new array with the specified element size and length.
@@ -60,18 +62,21 @@ void *array_get(const array *arr, size_t index);
  * @param length The initial length of the array.
  * @return A pointer to the initialized array, or NULL if allocation fails.
  */
-array *array_init(size_t elem_size, size_t length);
+array array_init(size_t elem_size, size_t length);
 
 /**
- * @brief Inserts an element into the array at the specified index.
+ * @brief Inserts an element into an array at the specified index.
  *
  * @param arr The array to insert the element into.
  * @param elem The element to insert.
  * @param index The index to insert the element at.
  * @return A pointer to the inserted element, or NULL if the array is full or
  * the index is out of bounds.
+ * @note Every element after the insertion index will be shifted over by one
+ * index except for the last element, which will be overwritten by the
+ * second-to-last element.
  */
-void *array_insert(array *arr, const void *elem, size_t index);
+void *array_insert_(array *arr, const void *elem, size_t index);
 
 /**
  * @brief Creates a new array from the given data.
@@ -81,6 +86,15 @@ void *array_insert(array *arr, const void *elem, size_t index);
  * @param length The length of the data.
  * @return A pointer to the new array, or NULL if allocation fails.
  */
-array *array_new_(const void *data, size_t elem_size, size_t length);
+array array_new_(const void *data, size_t elem_size, size_t length);
+
+/**
+ * @brief Sets an element in an array.
+ *
+ * @param arr The array to add the element to.
+ * @param elem The element to add.
+ * @return A pointer to the added element, or NULL if the array is full.
+ */
+void *array_set_(array *arr, const void *elem, size_t index);
 
 #endif
