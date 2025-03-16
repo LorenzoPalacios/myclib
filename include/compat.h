750b6da6c77dc7f9bf28d1aceba19933e927a435
LorenzoPalacios <lp902470@gmail.com>
2025-03-16 00:30:25 -0500

    include: fix compat.h preprocessor conditional
    Signed-off-by: LorenzoPalacios <lp902470@gmail.com>

--- include/compat.h
+++ include/compat.h
@@ -3,7 +3,7 @@
 
 #ifdef __cplusplus
 #define restrict /* `restrict` keyword is not present in C++ or versions of C prior to C99. */
-#elif !(defined __STDC_VERSION__) || (__STDC_VERSION__ >= 199901L)
+#elif !(defined __STDC_VERSION__) || (__STDC_VERSION__ < 199901L)
 
 #define inline   /* `inline` keyword is not standardized prior to C99. */
 #define restrict /* `restrict` keyword is not standardized prior to C99. */
