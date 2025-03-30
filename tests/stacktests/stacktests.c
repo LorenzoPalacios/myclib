8626c6b4fb2b7a6fff51c0cbe085e1ca88225466
LorenzoPalacios <lp902470@gmail.com>
2025-03-30 18:08:49 -0500

    stacktests.c: fixed test logic
    Signed-off-by: LorenzoPalacios <lp902470@gmail.com>

--- tests/stacktests/stacktests.c
+++ tests/stacktests/stacktests.c
@@ -2,44 +2,26 @@
 
 #include <assert.h>
 #include <stddef.h>
+#include <stdio.h>
 
 #include "../../stack/stack.h"
 
-#define ARR_LEN(arr) (sizeof(arr) / sizeof *(arr))
+static const int TEST_ARR[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+static const size_t TEST_ARR_LEN = sizeof(TEST_ARR) / sizeof *(TEST_ARR);
 
 void test_stack_copy(void) {
-  const int arr[] = {1, 2, 3, 4, 5};
-  stack(stack(int)) stk1 = stack_init(stack(int), 3);
-  stack(stack(int)) stk1_copy;
-
-  stack_push_s(stk1, stack_new(arr));
-  stack_push_s(stk1, stack_new(arr));
-  stack_push_s(stk1, stack_new(arr));
-
-  stk1_copy = stack_copy(stk1);
+  stack(int) stk1 = stack_new(TEST_ARR);
+  stack(int) stk1_copy = stack_copy(stk1);
 
   assert(stack_capacity(stk1) == stack_capacity(stk1_copy));
   assert(stack_height(stk1) == stack_height(stk1_copy));
-  assert(stack_height(stk1) == ARR_LEN(arr));
+  assert(stack_height(stk1) == TEST_ARR_LEN);
 
   while (!stack_is_empty(stk1)) {
-    stack(int) int_stk = *stack_pop(stk1);
-    stack(int) int_stk_copy = *stack_pop(stk1_copy);
-    size_t i = ARR_LEN(arr) - 1;
-
-    /*
-     * This is a pointer comparison; `stack_copy` does not perform a deep
-     * copy.
-     */
-    assert(int_stk == int_stk_copy);
-
-    for (; !stack_is_empty(int_stk); i++) {
-      const int int_original_val = arr[i];
-      const int int_stk_val = *stack_pop(int_stk);
-      const int int_stk_copy_val = *stack_pop(int_stk_copy);
-      assert(int_original_val == int_stk_val);
-      assert(int_stk_val == int_stk_copy_val);
-    }
+    const int num = *stack_pop(stk1);
+    const int num_copy = *stack_pop(stk1_copy);
+    assert(num == num_copy);
   }
 
   stack_delete(stk1);
@@ -83,24 +65,22 @@ void test_stack_init(void) {
 }
 
 void test_stack_new(void) {
-  const int arr[] = {1, 2, 3};
-  stack(int) stk = stack_new(arr);
+  stack(int) stk = stack_new(TEST_ARR);
 
-  size_t i = ARR_LEN(arr) - 1;
-  for (; !stack_is_empty(stk); i++) {
+  size_t i = TEST_ARR_LEN-1;
+  for (; !stack_is_empty(stk); i--) {
     const int stk_value = *stack_pop(stk);
-    assert(stk_value == arr[i]);
+    assert(stk_value == TEST_ARR[i]);
   }
 
   stack_delete(stk);
 }
 
 void test_stack_peek(void) {
-  const int arr[] = {1, 2, 3};
-  stack(int) stk1 = stack_new(arr);
+  stack(int) stk1 = stack_new(TEST_ARR);
   stack(int) stk2 = stack_init(int, 1);
 
-  assert(*stack_peek(stk1) == arr[ARR_LEN(arr) - 1]);
+  assert(*stack_peek(stk1) == TEST_ARR[TEST_ARR_LEN - 1]);
   assert(stack_peek_s(stk2) == NULL);
 
   stack_delete(stk1);
@@ -108,13 +88,12 @@ void test_stack_peek(void) {
 }
 
 void test_stack_pop(void) {
-  const int arr[] = {1, 2, 3};
-  stack(int) stk1 = stack_new(arr);
+  stack(int) stk1 = stack_new(TEST_ARR);
   stack(int) stk2 = stack_init(int, 1);
 
   const int stk1_value = *stack_pop(stk1);
   const int *const stk2_value = stack_pop_s(stk2);
-  assert(stk1_value == arr[ARR_LEN(arr) - 1]);
+  assert(stk1_value == TEST_ARR[TEST_ARR_LEN - 1]);
   assert(stk2_value == NULL);
 
   stack_delete(stk1);
@@ -122,27 +101,26 @@ void test_stack_pop(void) {
 }
 
 void test_stack_push(void) {
-  const int arr[] = {1, 2, 3};
   stack(int) stk = stack_init(int, 0);
   const size_t INITIAL_HEIGHT = stack_height(stk);
 
   {
     size_t i = 0;
-    for (; i < ARR_LEN(arr); i++) stack_push(stk, arr + i);
+    for (; i < TEST_ARR_LEN; i++) stack_push(stk, TEST_ARR + i);
   }
 
   assert(stack_height(stk) != INITIAL_HEIGHT);
 
   {
     size_t i = 0;
-    while (i < ARR_LEN(arr)) stack_push_s(stk, arr + i++);
+    for (; i < TEST_ARR_LEN; i++) stack_push_s(stk, TEST_ARR + i);
   }
 
   while (!stack_is_empty(stk)) {
     size_t i = 0;
-    for (; i < ARR_LEN(arr); i++) {
+    for (; i < TEST_ARR_LEN; i++) {
       const int stk_value = *stack_pop(stk);
-      assert(stk_value == arr[i]);
+      assert(stk_value == TEST_ARR[TEST_ARR_LEN - i - 1]);
     }
   }
 
@@ -150,8 +128,7 @@ void test_stack_push(void) {
 }
 
 void test_stack_reset(void) {
-  const int arr[] = {1, 2, 3};
-  stack(int) stk1 = stack_new(arr);
+  stack(int) stk1 = stack_new(TEST_ARR);
 
   assert(!stack_is_empty(stk1));
   stack_reset(stk1);
