d11215565687249c80791ed5e9f9286eb6a98340
LorenzoPalacios <lp902470@gmail.com>
2025-04-14 20:54:08 -0500

    utilmyclib.h: add typedef for byte, refactored macros
    Signed-off-by: LorenzoPalacios <lp902470@gmail.com>

--- include/utilmyclib.h
+++ include/utilmyclib.h
@@ -3,25 +3,27 @@
 #include <stdio.h>
 #include <stdlib.h> /* For abort(). */
 
+typedef unsigned char byte;
+
 /* Aborts the program if `expr` evaluates to false. */
 #define util_assert(expr) \
-  ((void)((expr) ? 0 : util_assert_msg(expr), fflush(stderr), abort()))
+  inline_if(expr, NULL, (util_assert_msg(expr), abort()))
 
-#if (__STDC_VERSION__ >= 199901L)
+#if (defined __STDC_VERSION__ && __STDC_VERSION__ >= 199901L)
 #define util_assert_msg(expr)                                                 \
   fprintf(stderr,                                                             \
-          "\n%s:%zu in function %s -\nExpression (%s) evaluated to false!\n", \
+          "\n%s:%zu in function %s - Expression\n\n%s\n\nevaluated to false!\n", \
           __FILE__, (size_t)__LINE__, __func__, STRINGIFY(expr)),             \
       fflush(stderr)
 #else
-#define util_assert_msg(expr)                                          \
-  fprintf(stderr, "\n%s:%zu -\nExpression (%s) evaluated to false!\n", \
-          __FILE__, (size_t)__LINE__, STRINGIFY(expr)),                \
+#define util_assert_msg(expr)                                               \
+  fprintf(stderr, "\n%s:%zu - Expression evaluated to false!\n", __FILE__, \
+          (size_t)__LINE__),                                                \
       fflush(stderr)
 #endif
 
 #define inline_if(cond, true_branch_expr, false_branch_expr) \
-  ((void)((cond) ? (true_branch_expr) : (false_branch_expr)))
+  (((cond) ? (void)(true_branch_expr) : (void)(false_branch_expr)))
 
 #ifndef STRINGIFY
 #define STRINGIFY(x) #x
