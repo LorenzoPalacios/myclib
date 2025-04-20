21d13287ea3c8bc78fbac9c692e6d845cb22163f
LorenzoPalacios <lp902470@gmail.com>
2025-04-20 09:06:47 -0500

    utilmyclib.h: add cnt_digits
    revised util_assert_msg to use fputs
    
    Signed-off-by: LorenzoPalacios <lp902470@gmail.com>

--- include/utilmyclib.h
+++ include/utilmyclib.h
@@ -1,5 +1,6 @@
 #ifndef UTILMYCLIB_H
 
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h> /* For abort(). */
 
@@ -10,20 +11,45 @@ typedef unsigned char byte;
   inline_if(expr, NULL, (util_assert_msg(expr), abort()))
 
 #if (defined __STDC_VERSION__ && __STDC_VERSION__ >= 199901L)
-#define util_assert_msg(expr)                                                 \
-  fprintf(stderr,                                                             \
-          "\n%s:%zu in function %s - Expression\n\n%s\n\nevaluated to false!\n", \
-          __FILE__, (size_t)__LINE__, __func__, STRINGIFY(expr)),             \
-      fflush(stderr)
+#define util_assert_msg(expr)                                              \
+  ((fputs("\n"__FILE__                                                     \
+          ":" STRINGIFY(__LINE__) " in function ",                         \
+          stderr),                                                         \
+    fputs(__func__, stderr),                                               \
+    fputs(" - Expression\n\n" STRINGIFY(expr) "\n\nevaluated to false!\n", \
+          stderr),                                                         \
+    fflush(stderr)))
 #else
-#define util_assert_msg(expr)                                               \
-  fprintf(stderr, "\n%s:%zu - Expression evaluated to false!\n", __FILE__, \
-          (size_t)__LINE__),                                                \
+#define util_assert_msg(expr)                                        \
+  fputs(                                                             \
+      "\n"__FILE__                                                   \
+      ":" STRINGIFY(__LINE__) " - Expression evaluated to false!\n", \
+      stderr),                                                       \
       fflush(stderr)
 #endif
 
 #define inline_if(cond, true_branch_expr, false_branch_expr) \
   (((cond) ? (void)(true_branch_expr) : (void)(false_branch_expr)))
+
+#if (defined __STDC_VERSION__ && __STDC_VERSION__ >= 199901L)
+#define cnt_digits(n) \
+  (cnt_digits_((n) < 0 ? ULLONG_MAX - (unsigned long long)(n) + 1 : (n)))
+
+static inline size_t cnt_digits_(unsigned long long n) {
+  size_t digits = 1;
+  while (n /= 10) digits++;
+  return digits;
+}
+#else
+#define cnt_digits(n) \
+  (cnt_digits_((n) < 0 ? ULLONG_MAX - (unsigned long)(n) + 1 : (n)))
+
+static inline size_t cnt_digits_(unsigned long n) {
+  size_t digits = 1;
+  while (n /= 10) digits++;
+  return digits;
+}
+#endif
 
 #ifndef STRINGIFY
 #define STRINGIFY(x) #x
