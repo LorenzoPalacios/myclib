4804a362711f1d921b62561d39854f27bbfe0c57
LorenzoPalacios <lp902470@gmail.com>
2025-04-22 19:14:29 -0500

    utilmyclib.h: refactored
    removed cnt_digits
    
    added macros to check __STDC_VERSION__
    
    added typedef for widest basic integral types (wb_int, wb_uint)
    
    Signed-off-by: LorenzoPalacios <lp902470@gmail.com>

--- include/utilmyclib.h
+++ include/utilmyclib.h
@@ -1,36 +1,34 @@
 #ifndef UTILMYCLIB_H
 #define UTILMYCLIB_H
 
-#include <limits.h> /* For ULLONG_MAX, ULONG_MAX. */
+#include <limits.h>
 #include <stddef.h> /* For NULL, size_t. */
 #include <stdio.h>  /* For fputs(), stderr. */
 #include <stdlib.h> /* For abort(). */
 
+#define STDC95_VERSION 199409L
+#define STDC99_VERSION 199901L
+#define STDC11_VERSION 201112L
+#define STDC17_VERSION 201710L
+#define STDC23_VERSION 202311L
+
+#ifdef __STDC_VERSION__
+#define IS_STDC95 (__STDC_VERSION__ >= STDC95_VERSION)
+#define IS_STDC99 (__STDC_VERSION__ >= STDC99_VERSION)
+#define IS_STDC11 (__STDC_VERSION__ >= STDC11_VERSION)
+#define IS_STDC17 (__STDC_VERSION__ >= STDC17_VERSION)
+#define IS_STDC23 (__STDC_VERSION__ >= STDC23_VERSION)
+#else
+#define IS_STDC95 (0)
+#define IS_STDC99 (0)
+#define IS_STDC11 (0)
+#define IS_STDC17 (0)
+#define IS_STDC23 (0)
+#endif
+
 typedef unsigned char byte;
 
 #define ARR_LEN(arr) (sizeof(arr) / sizeof *(arr))
-
-#if (defined __STDC_VERSION__ && __STDC_VERSION__ >= 199901L)
-#define cnt_digits(n)                                              \
-  (cnt_digits_(((n) < 0 ? ULLONG_MAX - (unsigned long long)(n) + 1 \
-                        : (unsigned long long)(n))))
-
-static inline size_t cnt_digits_(unsigned long long n) {
-  size_t digits = 1;
-  while (n /= 10) digits++;
-  return digits;
-}
-#else
-#define cnt_digits(n) \
-  (cnt_digits_(       \
-      ((n) < 0 ? ULONG_MAX - (unsigned long)(n) + 1 : (unsigned long)(n))))
-
-static size_t cnt_digits_(unsigned long n) {
-  size_t digits = 1;
-  while (n /= 10) digits++;
-  return digits;
-}
-#endif
 
 #define inline_if(cond, true_branch_expr, false_branch_expr) \
   (((cond) ? (void)(true_branch_expr) : (void)(false_branch_expr)))
@@ -41,7 +39,7 @@ static size_t cnt_digits_(unsigned long n) {
 #define util_assert(expr) \
   inline_if(expr, NULL, (util_assert_msg(expr), abort()))
 
-#if (defined __STDC_VERSION__ && __STDC_VERSION__ >= 199901L)
+#if (IS_STDC99)
 #define util_assert_msg(expr)                                              \
   ((fputs("\n"__FILE__                                                     \
           ":" STRINGIFY(__LINE__) " in function ",                         \
@@ -50,6 +48,18 @@ static size_t cnt_digits_(unsigned long n) {
     fputs(" - Expression\n\n" STRINGIFY(expr) "\n\nevaluated to false!\n", \
           stderr),                                                         \
     fflush(stderr)))
+
+/* Widest basic integral type. */
+typedef long long wb_int;
+/* Widest basic unsigned integral type. */
+typedef unsigned long long wb_uint;
+
+#define WB_INT_FMT "%lld"
+#define WB_UINT_FMT "%llu"
+
+#define WB_INT_MAX (LLONG_MAX)
+#define WB_UINT_MAX (ULLONG_MAX)
+#define WB_INT_MIN (LLONG_MIN)
 #else
 #define util_assert_msg(expr)                                        \
   fputs(                                                             \
@@ -57,6 +67,18 @@ static size_t cnt_digits_(unsigned long n) {
       ":" STRINGIFY(__LINE__) " - Expression evaluated to false!\n", \
       stderr),                                                       \
       fflush(stderr)
+
+/* Widest basic integral type. */
+typedef long wb_int;
+/* Widest basic unsigned integral type. */
+typedef unsigned long wb_uint;
+
+#define WB_INT_FMT "%ld"
+#define WB_UINT_FMT "%lu"
+
+#define WB_INT_MAX (LONG_MAX)
+#define WB_UINT_MAX (ULONG_MAX)
+#define WB_INT_MIN (LONG_MIN)
 #endif
 
 #ifndef STRINGIFY
