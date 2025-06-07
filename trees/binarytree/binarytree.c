4f657937871fcaa8cddaa1e3e5ca6da3d2e6a485
LorenzoPalacios <lp902470@gmail.com>
2025-06-07 00:09:24 -0500

    binary tree: checkpoint commit and WIP
    Signed-off-by: LorenzoPalacios <lp902470@gmail.com>

--- trees/binarytree/binarytree.c
+++ trees/binarytree/binarytree.c
@@ -1,5 +1,6 @@
 #include "binarytree.h"
 
+#include <stdarg.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,50 +8,33 @@
 #include "../../include/myclib.h"
 #include "../../stack/stack.h"
 
-#define bt_calc_padding_bytes(capacity, value_size) \
-  (sizeof(size_t) - ((value_size) * (capacity) % sizeof(size_t)))
+/* - CONVENIENCE MACROS - */
+
+#define bt_calc_padding_init(capacity, value_size) \
+  ((value_size) == sizeof(size_t)                  \
+       ? 0                                         \
+       : sizeof(size_t) - ((value_size) * (capacity) % sizeof(size_t)))
 
 #define bt_header_from_ref(ref) (((bt_header) * (void **)(ref)) - 1)
 
-/* - INTERNAL FUNCTION DECLARATIONS - */
+#define bt_node_get_child_slot(node, should_take_left)                      \
+  ((should_take_left) ? ((node)->left == NULL_INDEX ? &(node)->left : NULL) \
+                      : ((node)->right == NULL_INDEX ? &(node)->right : NULL))
 
-static size_t bt_untyped_nv_pair_size(size_t value_size);
+/* - FUNCTION DECLARATIONS - */
 
+static bt_node bt_untyped_get_node(void *tree, size_t index, size_t value_size);
 static bt_node bt_untyped_node_arena(void *tree, size_t value_size);
-
+static size_t bt_untyped_nv_pair_size(size_t value_size);
 static size_t bt_untyped_total_nodes(const void *tree, size_t value_size);
+static bt_node bt_untyped_get_unused_node(void *tree, size_t value_size);
 
-/* - INTERNAL FUNCTION DEFINITIONS - */
+/* - TREE INTERNAL - */
 
-static inline size_t bt_cur_padding_bytes(const void *const tree, const size_t value_size) {
+static inline size_t bt_untyped_padding(const void *const tree,
+                                        const size_t value_size) {
   return bt_non_header_size(tree) - (bt_untyped_nv_pair_size(value_size) *
                                      bt_untyped_total_nodes(tree, value_size));
-}
-
-static inline bt_node bt_untyped_get_node(void *const tree, const size_t index,
-                                          const size_t value_size) {
-  return bt_untyped_node_arena(tree, value_size) + index;
-}
-
-static inline size_t bt_untyped_nv_pair_size(const size_t value_size) {
-  return sizeof(struct bt_node) + value_size;
-}
-
-static inline bt_node bt_untyped_node_arena(void *const tree,
-                                            const size_t value_size) {
-  return (bt_node)((byte *)tree + bt_cur_padding_bytes(tree, value_size) +
-                   (bt_untyped_total_nodes(tree, value_size) * value_size));
-}
-
-static inline size_t bt_untyped_total_nodes(const void *const tree,
-                                            const size_t value_size) {
-  return bt_non_header_size(tree) / bt_untyped_nv_pair_size(value_size);
-}
-
-static inline size_t bt_untyped_node_index(void *const tree, const_bt_node node,
-                                           const size_t value_size) {
-  return (size_t)(node -
-                  (const_bt_node)bt_untyped_node_arena(tree, value_size));
 }
 
 static inline bt_node bt_untyped_get_deleted_node(void *const tree,
@@ -61,9 +45,9 @@ static inline bt_node bt_untyped_get_deleted_node(void *const tree,
   return NULL;
 }
 
-static inline bt_node bt_untyped_get_unused_node(void *const tree,
-                                                 const size_t value_size) {
-  return bt_untyped_get_node(tree, bt_header(tree)->active_nodes, value_size);
+static inline bt_node bt_untyped_get_node(void *const tree, const size_t index,
+                                          const size_t value_size) {
+  return bt_untyped_node_arena(tree, value_size) + index;
 }
 
 static inline bt_node bt_untyped_get_open_node(void **const tree,
@@ -74,15 +58,71 @@ static inline bt_node bt_untyped_get_open_node(void **const tree,
   return candidate;
 }
 
-#define bt_node_get_child_slot(node, should_take_left)                      \
-  ((should_take_left) ? ((node)->left == NULL_INDEX ? &(node)->left : NULL) \
-                      : ((node)->right == NULL_INDEX ? &(node)->right : NULL))
+static inline bt_node bt_untyped_get_unused_node(void *const tree,
+                                                 const size_t value_size) {
+  return bt_untyped_get_node(tree, bt_header(tree)->active_nodes, value_size);
+}
+
+static inline bool bt_untyped_has_root(void *const tree) {
+  return bt_header(tree)->root != NULL_INDEX;
+}
+
+static inline bt_node bt_untyped_node_arena(void *const tree,
+                                            const size_t value_size) {
+  return (bt_node)((byte *)tree + bt_untyped_padding(tree, value_size) +
+                   (bt_untyped_total_nodes(tree, value_size) * value_size));
+}
+
+size_t bt_untyped_node_index(void *const tree, const_bt_node node,
+                             const size_t value_size) {
+  return (size_t)(node -
+                  (const_bt_node)bt_untyped_node_arena(tree, value_size));
+}
+
+static inline size_t bt_untyped_nv_pair_size(const size_t value_size) {
+  return sizeof(struct bt_node) + value_size;
+}
+
+static inline size_t bt_untyped_total_nodes(const void *const tree,
+                                            const size_t value_size) {
+  return bt_non_header_size(tree) / bt_untyped_nv_pair_size(value_size);
+}
+
+/* - NODE INTERNAL - */
+
+static inline bool bt_untyped_node_is_left(void *const tree,
+                                           const_bt_node child,
+                                           const_bt_node parent,
+                                           const size_t value_size) {
+  return parent->left == bt_untyped_node_index(tree, child, value_size);
+}
+
+static inline bt_node bt_untyped_node_parent(void *const tree,
+                                             const_bt_node node,
+                                             const size_t value_size) {
+  return bt_untyped_node_arena(tree, value_size) + node->parent;
+}
+
+static inline void bt_untyped_node_disconnect(void *const tree, bt_node node,
+                                              const size_t value_size) {
+  if (bt_has_parent(node)) {
+    bt_node parent = bt_untyped_node_parent(tree, node, value_size);
+    if (bt_untyped_node_is_left(tree, node, parent, value_size))
+      parent->left = NULL_INDEX;
+    else
+      parent->right = NULL_INDEX;
+    node->parent = NULL_INDEX;
+  }
+}
+
+static inline void bt_node_initialize(bt_node node) {
+  node->left = node->right = NULL_INDEX;
+}
 
 /* - FUNCTIONS - */
 
-bt_node bt_untyped_add_node(void **const tree, bt_node parent,
-                            const bool add_to_left, const void *const value,
-                            const size_t value_size) {
+bt_node bt_untyped_node_add(void **const tree, bt_node parent,
+                            const bool add_to_left, const size_t value_size) {
   void *const tree_actual = *tree;
   bt_header header = bt_header(tree_actual);
   bt_node new_node = bt_untyped_get_open_node(tree, value_size);
@@ -99,9 +139,22 @@ bt_node bt_untyped_add_node(void **const tree, bt_node parent,
     return NULL;
   }
   bt_node_initialize(new_node);
-  bt_untyped_set_value(tree_actual, new_node, value, value_size);
   header->active_nodes++;
   return new_node;
+}
+
+inline void bt_untyped_delete(void **const tree) {
+  bt_header header = bt_header_from_ref(tree);
+  stack_delete(header->deleted_nodes);
+  free(header);
+  *tree = NULL;
+}
+
+void bt_untyped_node_delete(void *const tree, bt_node node,
+                            const size_t value_size) {
+  stack_push(bt_header(tree)->deleted_nodes,
+             bt_untyped_node_index(tree, node, value_size));
+  bt_untyped_node_disconnect(tree, node, value_size);
 }
 
 void *bt_untyped_get_value(void *const tree, const_bt_node node,
@@ -110,8 +163,21 @@ void *bt_untyped_get_value(void *const tree, const_bt_node node,
          (value_size * bt_untyped_node_index(tree, node, value_size));
 }
 
+bt_node bt_untyped_left(void *const tree, const_bt_node node,
+                        const size_t value_size) {
+  return bt_untyped_node_arena(tree, value_size) + node->left;
+}
+
+bt_linkage bt_untyped_link_type(void *const tree, const_bt_node child,
+                                const_bt_node parent, const size_t value_size) {
+  const size_t CHILD_INDEX = bt_untyped_node_index(tree, child, value_size);
+  if (CHILD_INDEX == parent->left) return IS_LEFT;
+  if (CHILD_INDEX == parent->right) return IS_RIGHT;
+  return NO_LINK;
+}
+
 void *bt_untyped_new(const size_t capacity, const size_t value_size) {
-  const size_t PADDING = bt_calc_padding_bytes(capacity, value_size);
+  const size_t PADDING = bt_calc_padding_init(capacity, value_size);
   const size_t ALLOCATION = (bt_untyped_nv_pair_size(value_size) * capacity) +
                             PADDING + sizeof(struct bt_header);
   bt_header tree = malloc(ALLOCATION);
@@ -123,8 +189,54 @@ void *bt_untyped_new(const size_t capacity, const size_t value_size) {
   return tree + 1;
 }
 
+bt_node bt_untyped_parent(void *const tree, const_bt_node node,
+                          const size_t value_size) {
+  return bt_untyped_node_arena(tree, value_size) + node->parent;
+}
+
+bt_node bt_untyped_right(void *const tree, const_bt_node node,
+                         const size_t value_size) {
+  return bt_untyped_node_arena(tree, value_size) + node->right;
+}
+
+bt_node bt_untyped_root(void *const tree, const size_t value_size) {
+  return bt_untyped_has_root(tree)
+             ? bt_untyped_get_node(tree, bt_header(tree)->root, value_size)
+             : NULL;
+}
+
 void *bt_untyped_set_value(void *const tree, const_bt_node node,
                            const void *const value, const size_t value_size) {
   void *const dst = bt_untyped_get_value(tree, node, value_size);
   return memcpy(dst, value, value_size);
+}
+
+void bt_untyped_traverse(void **const tree_ref, bt_op op,
+                         const size_t value_size, void *args) {
+  void *tree = *tree_ref;
+  stack(size_t) branches = stack_new(size_t, bt_header(tree)->active_nodes / 3);
+
+  bt_node cur_node = bt_untyped_root(tree, value_size);
+  while (cur_node != NULL) {
+    op(tree_ref, cur_node, args);
+    if (bt_has_left(cur_node)) {
+      if (bt_has_right(cur_node)) {
+        const size_t CUR_NODE_INDEX =
+            bt_untyped_node_index(tree, cur_node, value_size);
+        stack_push_s(branches, CUR_NODE_INDEX);
+      }
+      cur_node = bt_untyped_left(tree, cur_node, value_size);
+    } else if (bt_has_right(cur_node)) {
+      cur_node = bt_untyped_right(tree, cur_node, value_size);
+    } else {
+      if (!stack_is_empty(branches)) {
+        const_bt_node prev =
+            bt_untyped_get_node(tree, stack_pop(branches), value_size);
+        cur_node = bt_untyped_right(tree, prev, value_size);
+      } else {
+        break;
+      }
+    }
+  }
+  stack_delete(branches);
 }
--- trees/binarytree/binarytree.h
+++ trees/binarytree/binarytree.h
@@ -34,114 +34,98 @@ typedef const struct bt_node *const_bt_node;
 
 typedef const struct bt_header *const_bt_header;
 
+typedef void (*bt_op)(binary_tree(void) *, bt_node, void *args);
+
+typedef enum {
+  IS_LEFT = 0,
+  IS_RIGHT = IS_LEFT + 1,
+  NO_LINK = IS_RIGHT + 1
+} bt_linkage;
+
 #define NULL_INDEX ((size_t)-1)
 
 /* - TREE MANIPULATION - */
 
-#define bt_add_node_s(tree, parent, add_to_left, value)                \
-  bt_untyped_add_node((void **)&(tree), parent, add_to_left, &(value), \
-                      sizeof *(tree))
+#define bt_node_add(tree, parent, add_to_left) \
+  bt_untyped_node_add((void **)&(tree), parent, add_to_left, sizeof *(tree))
 
-#define bt_get_deleted_node(tree)                                     \
-  (!stack_is_empty(bt_header(tree))                                   \
-       ? bt_get_node(tree, stack_pop(bt_header(tree)->deleted_nodes)) \
-       : NULL)
+/* - NODE MACROS - */
+
+#define bt_has_left(node) ((node)->left != NULL_INDEX)
+
+#define bt_has_parent(node) ((node)->parent != NULL_INDEX)
+
+#define bt_has_right(node) ((node)->right != NULL_INDEX)
+
+#define bt_left(tree, node) bt_untyped_left(tree, node, sizeof *(tree))
+
+#define bt_node_index(tree, node) ((node) - bt_node_arena(tree))
+
+#define bt_parent(tree, node) bt_untyped_parent(tree, node, sizeof *(tree))
+
+#define bt_right(tree, node) bt_untyped_right(tree, node, sizeof *(tree))
+
+#define bt_value(tree, node) ((tree)[bt_node_index(tree, node)])
+
+/* - TREE MACROS - */
+
+#define const_bt_header(tree) ((const_bt_header)(tree) - 1)
 
 #define bt_get_node(tree, index) (bt_node_arena(tree) + (index))
 
-#define bt_get_open_node(tree)                                        \
-  (!stack_is_empty((tree)->deleted_nodes) ? bt_get_deleted_node(tree) \
-                                          : bt_get_unused_node(tree))
-
-#define bt_get_unused_node(tree)                          \
-  ((bt_header(tree))->active_nodes < bt_total_nodes(tree) \
-       ? bt_get_node(tree, bt_header(tree)->active_nodes) \
-       : NULL)
-
-#define bt_has_root(tree) (bt_header(tree)->root != NULL_INDEX)
+#define bt_has_root(tree) (const_bt_header(tree)->root != NULL_INDEX)
 
 #define bt_header(tree) ((bt_header)(tree) - 1)
-
-#define bt_header_const(tree) ((const struct bt_header *)(tree) - 1)
-
-#define bt_padding(tree) \
-  (sizeof(size_t) - ((sizeof *(tree) * bt_total_nodes(tree)) % sizeof(size_t)))
-
-#define bt_root_node(tree) \
-  (bt_has_root(tree) ? bt_get_node(tree, bt_header(tree)->root) : NULL)
-
-#define bt_total_nodes(tree) (bt_non_header_size(tree) / bt_nv_pair_size(tree))
 
 #define bt_node_arena(tree) \
   ((bt_node)((byte *)((tree) + bt_total_nodes(tree)) + bt_padding(tree)))
 
 #define bt_non_header_size(tree) \
-  (bt_header_const(tree)->allocation - sizeof(struct bt_header))
+  (const_bt_header(tree)->allocation - sizeof(struct bt_header))
 
 #define bt_nv_pair_size(tree) (sizeof(struct bt_node) + sizeof *(tree))
 
-/* - NODE MANIPULATION - */
+#define bt_padding(tree) \
+  (bt_non_header_size(tree) - (bt_total_nodes(tree) * bt_nv_pair_size(tree)))
 
-#define bt_node_delete(tree, node)                                       \
-  stack_push(bt_header(tree)->deleted_nodes, bt_node_index(tree, node)); \
-  bt_node_disconnect(tree, node)
+#define bt_root(tree) (bt_get_node(tree, bt_header(tree)->root))
 
-#define bt_node_disconnect(tree, node)                           \
-  if (bt_node_has_parent(node)) {                                \
-    if (bt_node_is_left(tree, node, bt_node_parent(tree, node))) \
-      bt_node_parent(tree, node)->left = NULL_INDEX;             \
-    else                                                         \
-      bt_node_parent(tree, node)->right = NULL_INDEX;            \
-  }                                                              \
-  (void)0
+#define bt_root_s(tree) bt_untyped_root(tree, sizeof *(tree))
 
-#define bt_node_get_value(tree, node) ((tree)[bt_node_index(tree, node)])
+#define bt_total_nodes(tree) (bt_non_header_size(tree) / bt_nv_pair_size(tree))
 
-#define bt_node_has_left(node) ((node)->left != NULL_INDEX)
-
-#define bt_node_has_parent(node) ((node)->parent != NULL_INDEX)
-
-#define bt_node_has_right(node) ((node)->right != NULL_INDEX)
-
-#define bt_node_index(tree, node) ((size_t)((node) - bt_node_arena(tree)))
-
-#define bt_node_initialize(node) \
-  (void)((node)->left = (node)->right = NULL_INDEX)
-
-#define bt_node_is_root(tree, node) \
-  (bt_node_arena(tree) + bt_header(tree)->root == (node))
-
-#define bt_node_is_left(tree, child, parent) \
-  ((parent)->left == bt_node_index(tree, child))
-
-#define bt_node_is_right(tree, child, parent) \
-  ((parent)->right == bt_node_index(tree, child))
-
-#define bt_node_left(tree, node) \
-  (bt_node_has_left(node) ? bt_get_node(tree, (node)->left) : NULL)
-
-#define bt_node_right(tree, node) \
-  (bt_node_has_right(node) ? bt_get_node(tree, (node)->right) : NULL)
-
-#define bt_node_parent(tree, node) \
-  (bt_node_has_parent(node) ? bt_get_node(tree, (node)->right) : NULL)
-
-#define bt_node_value(tree, node) ((tree)[bt_node_index(tree, node)])
+#define bt_traverse(tree, op, args) \
+  bt_untyped_traverse((void **)&(tree), op, sizeof *(tree), args)
 
 /* - FUNCTIONS - */
 
-bt_node bt_untyped_add_node(binary_tree(void) *, bt_node parent,
-                            bool add_to_left, const void *value,
-                            size_t value_size);
+bt_node bt_untyped_node_add(binary_tree(void) *, bt_node parent,
+                            bool add_to_left, size_t value_size);
+
+void bt_untyped_delete(binary_tree(void) *);
 
 void *bt_untyped_get_value(binary_tree(void), const_bt_node node,
                            size_t value_size);
 
-void *bt_untyped_set_value(binary_tree(void), const_bt_node, const void *value,
-                           size_t value_size);
+bt_node bt_untyped_left(binary_tree(void), const_bt_node, size_t value_size);
+
+bt_linkage bt_untyped_link_type(void *tree, const_bt_node child,
+                                const_bt_node parent, size_t value_size);
+
+void bt_untyped_node_delete(binary_tree(void), bt_node, size_t value_size);
 
 binary_tree(void) bt_untyped_new(size_t capacity, size_t value_size);
 
-void bt_untyped_traverse(binary_tree(void), size_t value_size);
+bt_node bt_untyped_parent(binary_tree(void), const_bt_node, size_t value_size);
+
+bt_node bt_untyped_right(binary_tree(void), const_bt_node, size_t value_size);
+
+bt_node bt_untyped_root(binary_tree(void), size_t value_size);
+
+void *bt_untyped_set_value(binary_tree(void), const_bt_node, const void *value,
+                           size_t value_size);
+
+void bt_untyped_traverse(binary_tree(void) *, bt_op, size_t value_size,
+                         void *args);
 
 #endif
